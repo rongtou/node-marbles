00b61f6710e09fca5fdeba1270007f04f73ef209
rongtou <rongtou@gmail.com>
2011-07-03 10:42:39 +0800

    width fixed

--- src/wp/canvas.js
+++ src/wp/canvas.js
@@ -3,7 +3,7 @@ $(window).ready(function() {
 });
 
 function createCanvas(){
-    var canvasStr = '<canvas id="marbleTable" width="600" height="400">' +
+    var canvasStr = '<canvas id="marbleTable" width="550" height="400">' +
 	    	"Your browser doesn't support HTML5" +
 	    '</canvas>';
     $("#marbleContainer").append(canvasStr);
